cfd39f9e18e935231f43210478f84216bf406571
jjipper <91707346+jjipper@users.noreply.github.com>
2026-02-11 18:47:11 +0900

    chore(project): 기본 프로젝트 구조 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App/App.tsx';
+import './main.scss';
+import App from './features/App/App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
