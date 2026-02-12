d6462d2158fb7a19d74967733cbae4949b1ad3c8
jjipper <91707346+jjipper@users.noreply.github.com>
2026-02-12 17:59:06 +0900

    chore(project): Main import 파일 정리

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,16 +1,9 @@
-// import { useEffect, useRef, useState } from 'react';
-import type { FunctionComponent } from 'react';
 import IntroSection from '../../features/IntroSection/IntroSection';
 import IssueSection from '../../features/IssueSection/IssueSection';
 import MyStockSection from '../../features/MyStockSection/MyStockSection';
 import HotStockSection from '../../features/HotStockSection/HotStockSection';
-import './MainPage.module.scss';
 
-// interface DetailProps {
-//   id: string;
-// }
-
-const MainPage: FunctionComponent = () => {
+const MainPage = () => {
   return (
     <>
       <IntroSection />
