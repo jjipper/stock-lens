849a17f095403e9abe339bc560604dee9d738737
jjipper <91707346+jjipper@users.noreply.github.com>
2026-02-13 00:54:00 +0900

    feat(project): Stock Chips 컴포넌트 구축 및 스타일 적용 완료

---
+++ src/shared/StockChips/StockChips.tsx
@@ -0,0 +1,33 @@
+import { Chips, Chip } from './ui/styled';
+
+interface StockChipsProps {
+  stocks: { name: string; ticker: string; percentage: number }[];
+  showTicker: boolean;
+}
+
+const StockChips = ({ stocks, showTicker }: StockChipsProps) => {
+  function StockType(percentage: number) {
+    return percentage > 0 ? 'up' : 'down';
+  }
+
+  return (
+    <Chips>
+      {stocks.map((stock) => {
+        return (
+          <Chip key={stock.ticker}>
+            <div className="name-ticker">
+              <span className="name">{stock.name}</span>
+              {showTicker && <span className="ticker">{stock.ticker}</span>}
+            </div>
+            <span className={`percentage ${StockType(stock.percentage)}`}>
+              {StockType(stock.percentage) === 'up' && '+'}
+              {stock.percentage} %
+            </span>
+          </Chip>
+        );
+      })}
+    </Chips>
+  );
+};
+
+export default StockChips;
