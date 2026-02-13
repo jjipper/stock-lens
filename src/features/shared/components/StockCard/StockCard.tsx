745f13994bcacf2155a3758931df59185944cab2
jjipper <91707346+jjipper@users.noreply.github.com>
2026-02-13 15:03:11 +0900

    feat(project): Stock Card 컴포넌트 구축 및 스타일 적용 완료

---
+++ src/features/shared/components/StockCard/StockCard.tsx
@@ -0,0 +1,45 @@
+import { Card } from './ui/styled';
+import { TrendingUpIcon, TrendingDownIcon } from '../../ui/Icons/Icons';
+
+interface StockCardProps {
+  name: string;
+  ticker: string | number;
+  mainIssue?: string;
+  price: number;
+  percentage: number;
+}
+
+export const StockCard = ({
+  name,
+  ticker,
+  mainIssue,
+  price,
+  percentage,
+}: StockCardProps) => {
+  return (
+    <Card>
+      <dl className="stock-info">
+        <dt>
+          <span className="name">{name}</span>
+          <span className="ticker">{ticker}</span>
+        </dt>
+        <dd className="main-issue">{mainIssue}</dd>
+      </dl>
+      <div className="stock-price">
+        <span className="price">{price}원</span>
+        <span className={`percentage ${percentage > 0 ? 'up' : 'down'}`}>
+          {percentage > 0 ? (
+            <>
+              <TrendingUpIcon sx={{ fontSize: 12 }} />+
+            </>
+          ) : (
+            <TrendingDownIcon sx={{ fontSize: 12 }} />
+          )}
+          {percentage}%
+        </span>
+      </div>
+    </Card>
+  );
+};
+
+export default StockCard;
