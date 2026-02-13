e53400f5e727efd18c32823b9cd9f46e9c5a8c57
jjipper <91707346+jjipper@users.noreply.github.com>
2026-02-13 12:56:16 +0900

    chore(project): 파일 import 절대 경로로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      app: fileURLToPath(new URL('./src/features/app', import.meta.url)),
+      pages: fileURLToPath(new URL('./src/features/pages', import.meta.url)),
+      shared: fileURLToPath(new URL('./src/features/shared', import.meta.url)),
+      widgets: fileURLToPath(new URL('./src/features/widgets', import.meta.url)),
+    },
+  },
 });
