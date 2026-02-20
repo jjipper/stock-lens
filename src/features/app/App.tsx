f753132b3527e8907f19524309248e8e3660a251
jjipper <91707346+jjipper@users.noreply.github.com>
2026-02-20 15:51:04 +0900

    refactor(project): 전역 스타일 엔트리를 index.css로 전환하고 App 진입점에 연결

--- src/features/app/App.tsx
+++ src/features/app/App.tsx
@@ -1,13 +1,7 @@
 import { AppRoutes } from 'features/app/Routes/AppRoutes';
-import { GlobalStyles } from 'features/app/GlobalStyles';
 
 const App = () => {
-  return (
-    <>
-      <GlobalStyles />
-      <AppRoutes />
-    </>
-  );
+  return <AppRoutes />;
 };
 
 export default App;
