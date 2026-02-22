89ba006c4e36421a289920f7a2ab38604af2804f
jjipper <91707346+jjipper@users.noreply.github.com>
2026-02-22 23:29:51 +0900

    feat(project): MSW 워커/핸들러 파일 추가 및 초기화 설정

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,19 @@ import { createRoot } from 'react-dom/client';
 import App from 'features/app/App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+async function enableMocking() {
+  if (!import.meta.env.DEV) {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser');
+  await worker.start();
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+});
