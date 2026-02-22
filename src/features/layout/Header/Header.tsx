43ec42b0dfce241593abca620138c8d94f128128
jjipper <91707346+jjipper@users.noreply.github.com>
2026-02-22 18:04:39 +0900

    refactor(project): useNavigate를 Link로 교체

--- src/features/layout/Header/Header.tsx
+++ src/features/layout/Header/Header.tsx
@@ -1,25 +1,17 @@
-import { useNavigate, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  const handleLogoClick = () => {
-    if (location.pathname !== '/') {
-      navigate('/');
-    }
-  };
-
   return (
     <header className="flex h-16 items-center border-b border-gray-300 bg-white px-6">
-      <button
-        type="button"
-        onClick={handleLogoClick}
+      <h1>
+        <Link
+          to="/"
         aria-label="메인 페이지로 이동"
         className="cursor-pointer rounded-md border-none bg-transparent text-2xl font-bold"
       >
         Stock News Timeline
-      </button>
+        </Link>
+      </h1>
     </header>
   );
 };
