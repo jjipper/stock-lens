62edb1962fa17e559b22da040e7ef085bba9544e
jjipper <91707346+jjipper@users.noreply.github.com>
2026-02-23 14:16:55 +0900

    a11y(project): Header - Home Link 부분 h1 태그 제거

--- src/features/layout/Header/Header.tsx
+++ src/features/layout/Header/Header.tsx
@@ -3,15 +3,13 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header className="flex h-16 items-center border-b border-gray-200 bg-white px-6">
-      <h1>
-        <Link
-          to="/"
-          aria-label="Stock News Timeline, go to main page"
-          className="cursor-pointer rounded-md border-none bg-transparent text-2xl font-bold"
-        >
-          Stock News Timeline
-        </Link>
-      </h1>
+      <Link
+        to="/"
+        aria-label="Stock News Timeline, go to main page"
+        className="cursor-pointer rounded-md border-none bg-transparent text-2xl font-bold"
+      >
+        Stock News Timeline
+      </Link>
       <nav className="ml-auto flex gap-1">
         <Link
           to="/issue"
