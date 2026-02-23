316ef62e40b88afc09bf8e0c6d0c127462adf6df
jjipper <91707346+jjipper@users.noreply.github.com>
2026-02-23 16:26:04 +0900

    feat(project): SectionEmpty 파일 추가

---
+++ src/features/layout/SectionEmpty/SectionEmpty.tsx
@@ -0,0 +1,20 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'features/shared';
+
+const SectionEmpty = () => {
+  const navigate = useNavigate();
+  const handleMainClick = () => {
+    navigate('/');
+  };
+
+  return (
+    <div className="absolute top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-4 text-center">
+      <p className="text-xl text-gray-500">The page is empty</p>
+      <Button variant="primary" size="lg" onClick={handleMainClick}>
+        Go To Main
+      </Button>
+    </div>
+  );
+};
+
+export { SectionEmpty };
--- src/features/layout/index.ts
+++ src/features/layout/index.ts
@@ -4,3 +4,4 @@ export { MainLayout } from './MainLayout/MainLayout';
 export { SectionHeader } from './SectionHeader/SectionHeader';
 export { SectionWrapper } from './SectionWrapper/SectionWrapper';
 export { NotFoundPage } from './NotFoundPage/NotFoundPage';
+export { SectionEmpty } from './SectionEmpty/SectionEmpty';
