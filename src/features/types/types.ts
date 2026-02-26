ddac4fcf947b71449d286cbd65326d1fe9f048e1
진주 <91707346+jjipper@users.noreply.github.com>
2026-02-26 18:09:44 +0900

    chore(types): NewsListItem, NewsItem 타입 정의 추가

--- src/features/types/types.ts
+++ src/features/types/types.ts
@@ -1,50 +1,63 @@
 export type IssueType =
-  | 'semiconductor'
-  | 'battery'
-  | 'biotech'
-  | 'mobility'
-  | 'internet'
-  | 'energy'
-  | 'defense'
-  | 'finance';
+	| 'semiconductor'
+	| 'battery'
+	| 'biotech'
+	| 'mobility'
+	| 'internet'
+	| 'energy'
+	| 'defense'
+	| 'finance';
 
 export interface IssueCardProps {
-  id: string;
-  type: IssueType;
-  title: string;
-  description: string;
-  stocks: { name: string; ticker: string; percentage: number }[];
-  newsCount: number;
-  mention: boolean;
-  effect: 'high' | 'middle' | 'low';
+	id: string;
+	type: IssueType;
+	title: string;
+	description: string;
+	stocks: { name: string; ticker: string; percentage: number }[];
+	newsCount: number;
+	mention: boolean;
+	effect: 'high' | 'middle' | 'low';
 }
 
 export interface StockCardProps {
-  name: string;
-  ticker: string;
-  mainIssue?: string;
-  price: number;
-  percentage: number;
-  myStock: boolean;
-  popular: boolean;
-}
-
-export interface StockDetailCardProps extends StockCardProps {
-  priceRange: number;
-  lowPrice: number;
-  highPrice: number;
-  weekReturn: number;
-  monthReturn: number;
+	name: string;
+	ticker: string;
+	mainIssue?: string;
+	price: number;
+	percentage: number;
+	myStock: boolean;
+	popular: boolean;
 }
 
 export interface ButtonProps {
-  children: React.ReactNode;
-  variant?: 'primary' | 'line' | 'default';
-  size?: 'sm' | 'md' | 'lg';
-  onClick?: () => void;
-  className?: string;
+	children: React.ReactNode;
+	variant?: 'primary' | 'line' | 'default';
+	size?: 'sm' | 'md' | 'lg';
+	onClick?: () => void;
+	className?: string;
 }
 
 export interface AppBarOutletContext {
-  setAppBarTitle: (v: string) => void;
+	setAppBarTitle: (v: string) => void;
+}
+
+export interface NewsListItem {
+	id?: string;
+	title: string;
+	publishedAt: string;
+	source: string;
+	imageUrl: string;
+}
+
+export interface NewsItem {
+	id: string;
+	title: string;
+	publishedAt: string;
+	source: string;
+	imageUrl: string;
+	summary: [string, string, string];
+	impact: 'high' | 'middle' | 'low';
+	impactSummary: string;
+	nextCheckpoint: string;
+	articleUrl: string;
 }
