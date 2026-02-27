481d63efea68eb9739d94e88223a9f6a0332c7a0
진주 <91707346+jjipper@users.noreply.github.com>
2026-02-27 16:36:13 +0900

    chore: 코드 컨벤션에 맞게 TYPE_MET > typeMeta로 수정

--- src/features/Issues/TypeIcon/TypeIcon.tsx
+++ src/features/Issues/TypeIcon/TypeIcon.tsx
@@ -17,54 +17,54 @@ interface TypeIconProps {
 
 const DEFAULT_ICON_SIZE = 28;
 
-const TYPE_META = {
-  semiconductor: {
-    Icon: MemoryIcon,
-    wrapperClass: 'bg-semiconductor-bg text-semiconductor-icon',
-    iconSize: DEFAULT_ICON_SIZE,
-  },
-  battery: {
-    Icon: BatterySaverIcon,
-    wrapperClass: 'bg-battery-bg text-battery-icon',
-    iconSize: DEFAULT_ICON_SIZE,
-  },
-  biotech: {
-    Icon: BiotechIcon,
-    wrapperClass: 'bg-biotech-bg text-biotech-icon',
-    iconSize: DEFAULT_ICON_SIZE,
-  },
-  mobility: {
-    Icon: DirectionsCarFilledIcon,
-    wrapperClass: 'bg-mobility-bg text-mobility-icon',
-    iconSize: DEFAULT_ICON_SIZE,
-  },
-  internet: {
-    Icon: LanguageIcon,
-    wrapperClass: 'bg-internet-bg text-internet-icon',
-    iconSize: 26,
-  },
-  energy: {
-    Icon: BoltIcon,
-    wrapperClass: 'bg-energy-bg text-energy-icon',
-    iconSize: 30,
-  },
-  defense: {
-    Icon: GppGoodIcon,
-    wrapperClass: 'bg-defense-bg text-defense-icon',
-    iconSize: DEFAULT_ICON_SIZE,
-  },
-  finance: {
-    Icon: AccountBalanceIcon,
-    wrapperClass: 'bg-finance-bg text-finance-icon',
-    iconSize: 22,
-  },
+const typeMeta = {
+	semiconductor: {
+		Icon: MemoryIcon,
+		wrapperClass: 'bg-semiconductor-bg text-semiconductor-icon',
+		iconSize: DEFAULT_ICON_SIZE,
+	},
+	battery: {
+		Icon: BatterySaverIcon,
+		wrapperClass: 'bg-battery-bg text-battery-icon',
+		iconSize: DEFAULT_ICON_SIZE,
+	},
+	biotech: {
+		Icon: BiotechIcon,
+		wrapperClass: 'bg-biotech-bg text-biotech-icon',
+		iconSize: DEFAULT_ICON_SIZE,
+	},
+	mobility: {
+		Icon: DirectionsCarFilledIcon,
+		wrapperClass: 'bg-mobility-bg text-mobility-icon',
+		iconSize: DEFAULT_ICON_SIZE,
+	},
+	internet: {
+		Icon: LanguageIcon,
+		wrapperClass: 'bg-internet-bg text-internet-icon',
+		iconSize: 26,
+	},
+	energy: {
+		Icon: BoltIcon,
+		wrapperClass: 'bg-energy-bg text-energy-icon',
+		iconSize: 30,
+	},
+	defense: {
+		Icon: GppGoodIcon,
+		wrapperClass: 'bg-defense-bg text-defense-icon',
+		iconSize: DEFAULT_ICON_SIZE,
+	},
+	finance: {
+		Icon: AccountBalanceIcon,
+		wrapperClass: 'bg-finance-bg text-finance-icon',
+		iconSize: 22,
+	},
 } as const satisfies Record<
   IssueType,
   { Icon: SvgIconComponent; wrapperClass: string; iconSize: number }
 >;
 
 export const TypeIcon = ({ type }: TypeIconProps) => {
-  const { Icon, wrapperClass, iconSize } = TYPE_META[type];
+	const { Icon, wrapperClass, iconSize } = typeMeta[type];
 
   return (
     <div
