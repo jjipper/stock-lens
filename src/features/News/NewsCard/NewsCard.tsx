55a86e30de64e177056616689f487c7135d71d42
jjipper <91707346+jjipper@users.noreply.github.com.>
2026-03-01 03:39:35 +0900

    style(news): News Card img alt 추가, 스타일 조정

--- src/features/News/NewsCard/NewsCard.tsx
+++ src/features/News/NewsCard/NewsCard.tsx
@@ -5,22 +5,27 @@ export const NewsCard = ({
 	title,
 	source,
 	publishedAt,
-onClick,
+	onClick,
 }: NewsListItem) => {
 	return (
 		<button
 			type="button"
 			onClick={onClick}
-className="w-full cursor-pointer overflow-hidden rounded-xl border border-gray-100 bg-white text-left"
->
+			className="w-full cursor-pointer overflow-hidden rounded-xl border border-gray-100 bg-white text-left"
+		>
 			<div className="h-60 overflow-hidden">
-				<img src={imageUrl} />
+				<img
+					src={imageUrl}
+					alt={title}
+					className="h-full w-full object-cover"
+				/>
 			</div>
-			<div className="p-4">
+			<div className="flex flex-col gap-0.5 px-5 py-4">
 				<h3 className="text-md font-medium">{title}</h3>
 				<div className="flex items-center gap-2">
-					<p className="text-sm font-medium text-gray-700">{source}</p>
-					<p className="text-xs text-gray-500">{publishedAt}</p>
+					<p className="text-sm text-gray-600">{source}</p>
+					<span className="text-xs text-gray-300">|</span>
+					<p className="text-xs text-gray-600">{publishedAt}</p>
 				</div>
 			</div>
 		</button>
