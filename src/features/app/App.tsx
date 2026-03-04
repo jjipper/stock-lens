02584fc49ee0a1d5984f14bf1670f03b71006ff8
jjipper <91707346+jjipper@users.noreply.github.com.>
2026-03-05 01:30:25 +0900

    chore: App.tsx에 ReactQueryProvider 연결

--- src/features/app/App.tsx
+++ src/features/app/App.tsx
@@ -1,7 +1,13 @@
 import { AppRoutes } from 'features/app/Routes/AppRoutes';
 
+import { ReactQueryProvider } from './provider/ReactQueryProvider';
+
 const App = () => {
-	return <AppRoutes />;
+	return (
+		<ReactQueryProvider>
+			<AppRoutes />
+		</ReactQueryProvider>
+	);
 };
 
 export default App;
