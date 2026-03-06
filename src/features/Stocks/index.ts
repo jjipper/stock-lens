60541d134425b017239754ae1abb1acdf9320a84
jjipper <91707346+jjipper@users.noreply.github.com.>
2026-03-07 00:38:10 +0900

    chore(stocks): stocks public API 내보내기 추가

--- src/features/Stocks/index.ts
+++ src/features/Stocks/index.ts
@@ -1,3 +1,4 @@
-export { stocksAPI } from './api/stocksAPI';
+export { getStocks, stocksAPI } from './api/stocksAPI';
+export { useStocksQuery } from './hooks/useStocksQuery';
 export { StockCard } from './ui/StockCard/StockCard';
 export { StockDetailCard } from './ui/StockDetailCard/StockDetailCard';
