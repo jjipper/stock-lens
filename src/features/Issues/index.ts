0ed3ffd5301d6b4a56adfdeeaab28a2b3f09f263
jjipper <91707346+jjipper@users.noreply.github.com.>
2026-03-07 00:27:26 +0900

    chore(issues): issues public API 내보내기 추가

--- src/features/Issues/index.ts
+++ src/features/Issues/index.ts
@@ -1,3 +1,4 @@
-export { issuesAPI } from './api/issuesAPI';
+export { getIssues, issuesAPI } from './api/issuesAPI';
+export { useIssuesQuery } from './hooks/useIssuesQuery';
 export { IssueCard } from './ui/IssueCard/IssueCard';
 export { IssueDetailCard } from './ui/IssueDetailCard/IssueDetailCard';
