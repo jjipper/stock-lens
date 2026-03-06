69a48795e0fd8b0199f1188ad33e1a46cf4333c9
jjipper <91707346+jjipper@users.noreply.github.com.>
2026-03-07 03:10:30 +0900

    chore(stock): getStockDetail API 함수 추가

--- src/features/Stocks/api/stocksAPI.ts
+++ src/features/Stocks/api/stocksAPI.ts
@@ -8,3 +8,8 @@ export const getStocks = async () => {
 	const res = await stocksAPI.get('/');
 	return res.data;
 };
+
+export const getStockDetail = async (ticker: string) => {
+	const res = await stocksAPI.get(`/${ticker}`);
+	return res.data;
+};
