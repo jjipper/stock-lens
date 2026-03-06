cced409737f910b20978ece257bdcd9a0759cfbd
jjipper <91707346+jjipper@users.noreply.github.com.>
2026-03-07 01:34:20 +0900

    chore(news): getNewsByIds API 함수 추가

--- src/features/News/api/newsAPI.ts
+++ src/features/News/api/newsAPI.ts
@@ -3,3 +3,10 @@ import axios from 'axios';
 export const newsAPI = axios.create({
 	baseURL: '/news',
 });
+
+export const getNewsByIds = async (newsList: string[]) => {
+	const res = await newsAPI.get(
+		`?ids=${encodeURIComponent(newsList.join(','))}`,
+	);
+	return res.data;
+};
