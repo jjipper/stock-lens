85e4427bc70bb19f3cd8c9a3451b5665ac23ab1b
jjipper <91707346+jjipper@users.noreply.github.com.>
2026-03-07 03:08:10 +0900

    refactor(stocks): ErrorBoundary 추가하여 주식 목록 페이지 .isError 및 검증 코드 제거

--- src/features/pages/StockPage/StockPage.tsx
+++ src/features/pages/StockPage/StockPage.tsx
@@ -3,14 +3,13 @@ import { StockCard, useStocksQuery } from 'features/Stocks';
 import { type FunctionComponent } from 'react';
 
 export const StockPage: FunctionComponent = () => {
-	const query = useStocksQuery();
+	const { data } = useStocksQuery();
 
-	if (query.isError) return <div>Error: {query.error.message}</div>;
-	if (query.data?.length === 0) return <div>No Issues</div>;
+	if (data.length === 0) return <div>No Issues</div>;
 
 	return (
 		<TwoColumnGrid>
-			{query.data?.map((stock) => (
+			{data.map((stock) => (
 				<StockCard key={stock.ticker} {...stock} />
 			))}
 		</TwoColumnGrid>
