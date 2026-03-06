96639fcdad5a5b057567cda28bee80001deb525e
jjipper <91707346+jjipper@users.noreply.github.com.>
2026-03-07 00:25:36 +0900

    chore(issues): getIssues API 함수 추가

--- src/features/Issues/api/issuesAPI.ts
+++ src/features/Issues/api/issuesAPI.ts
@@ -3,3 +3,8 @@ import axios from 'axios';
 export const issuesAPI = axios.create({
 	baseURL: '/issues',
 });
+
+export const getIssues = async () => {
+	const res = await issuesAPI.get('/');
+	return res.data;
+};
