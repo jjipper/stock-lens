50b5f0853e5b1b7cab7cb3ab5ab437de07f14e49
jjipper <91707346+jjipper@users.noreply.github.com.>
2026-03-07 00:28:25 +0900

    refactor: 앱 라우터에 Suspense 및 lazy loading 적용

--- src/features/app/Routes/AppRoutes.tsx
+++ src/features/app/Routes/AppRoutes.tsx
@@ -1,33 +1,41 @@
 // import { ProtectedRoute } from './ProtectedRoute';
 import { AppBarLayout, MainLayout } from 'features/layout';
-import { IssueDetailPage } from 'features/pages/IssueDetailPage/IssueDetailPage';
-import { IssuePage } from 'features/pages/IssuePage/IssuePage';
-import { MainPage } from 'features/pages/MainPage/MainPage';
 import { NotFoundPage } from 'features/pages/NotFoundPage/NotFoundPage';
-import { StockDetailPage } from 'features/pages/StockDetailPage/StockDetailPage';
-import { StockPage } from 'features/pages/StockPage/StockPage';
+import { Loading } from 'features/shared';
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const MainPage = lazy(() => import('features/pages/MainPage/MainPage'));
+const IssuePage = lazy(() => import('features/pages/IssuePage/IssuePage'));
+const IssueDetailPage = lazy(
+	() => import('features/pages/IssueDetailPage/IssueDetailPage'),
+);
+const StockPage = lazy(() => import('features/pages/StockPage/StockPage'));
+const StockDetailPage = lazy(
+	() => import('features/pages/StockDetailPage/StockDetailPage'),
+);
 
 export const AppRoutes = () => {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<MainLayout />}>
-					<Route index element={<MainPage />} />
-					<Route path="*" element={<NotFoundPage />} />
-				</Route>
+			<Suspense fallback={<Loading />}>
+				<Routes>
+					<Route path="/" element={<MainLayout />}>
+						<Route index element={<MainPage />} />
+						<Route path="*" element={<NotFoundPage />} />
+					</Route>
 
-				<Route path="/issue" element={<AppBarLayout />}>
-					<Route index element={<IssuePage />} />
-					<Route path=":id" element={<IssueDetailPage />} />
-				</Route>
+					<Route path="/issue" element={<AppBarLayout />}>
+						<Route index element={<IssuePage />} />
+						<Route path=":id" element={<IssueDetailPage />} />
+					</Route>
 
-				<Route path="/stock" element={<AppBarLayout />}>
-					<Route index element={<StockPage />} />
-					<Route path=":ticker" element={<StockDetailPage />} />
-				</Route>
+					<Route path="/stock" element={<AppBarLayout />}>
+						<Route index element={<StockPage />} />
+						<Route path=":ticker" element={<StockDetailPage />} />
+					</Route>
 
-				{/* <Route
+					{/* <Route
           path="/admin"
           element={
             <ProtectedRoute>
@@ -35,7 +43,8 @@ export const AppRoutes = () => {
             </ProtectedRoute>
           }
         /> */}
-			</Routes>
+				</Routes>
+			</Suspense>
 		</BrowserRouter>
 	);
 };
