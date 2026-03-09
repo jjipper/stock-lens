f0f3eee04e9cb2efeb9df7473a4c7f270ddfc8fd
jjipper <91707346+jjipper@users.noreply.github.com.>
2026-03-10 01:35:22 +0900

    refactor(issue): IssueDetailPage에 ErrorProvider, Suspense 적용

--- src/features/pages/IssueDetailPage/IssueDetailPage.tsx
+++ src/features/pages/IssueDetailPage/IssueDetailPage.tsx
@@ -1,54 +1,24 @@
-import { IssueDetailCard, useIssueDetailQuery } from 'features/Issues';
-import { SectionHeader, TwoColumnGrid } from 'features/layout';
-import { useNewsQuery } from 'features/News';
-import { useNewsDetailQuery } from 'features/News/hooks/useNewsDetailQuery';
-import { NewsCard } from 'features/News/NewsCard/NewsCard';
-import { NewsModal } from 'features/News/NewsModal/NewsModal';
-import { type FunctionComponent, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { ErrorProvider } from 'features/app/provider/ErrorProvider';
+import { IssueDetailWrapper } from 'features/Issues';
+import { NewsListWrapper } from 'features/News';
+import { Loading } from 'features/shared';
+import { type FunctionComponent, Suspense } from 'react';
 
 export const IssueDetailPage: FunctionComponent = () => {
-	const { id } = useParams();
-	const [selectedNews, setSelectedNews] = useState('');
-	const { data: issue } = useIssueDetailQuery(id ?? '');
-	const { data: newsList } = useNewsQuery(issue.newsList);
-	const { data: newsItem } = useNewsDetailQuery(selectedNews);
-
-	if (!id) return <div>No Issue</div>;
-
 	return (
 		<>
 			<div className="w-full">
-				<ul className="flex flex-col gap-4">
-					<IssueDetailCard {...issue} />
-				</ul>
-
-				<div className="mt-6 flex flex-col gap-3">
-					<SectionHeader title="최신 뉴스" updateTime="2시간 전" />
-					<TwoColumnGrid>
-						{newsList.map((news) => {
-							return (
-								<NewsCard
-									key={news.id}
-									imageUrl={news.imageUrl}
-									title={news.title}
-									source={news.source}
-									publishedAt={news.publishedAt}
-									onClick={() => setSelectedNews(news.id)}
-								/>
-							);
-						})}
-					</TwoColumnGrid>
-				</div>
+				<ErrorProvider>
+					<Suspense fallback={<Loading />}>
+						<IssueDetailWrapper />
+					</Suspense>
+				</ErrorProvider>
+				<ErrorProvider>
+					<Suspense fallback={<Loading />}>
+						<NewsListWrapper />
+					</Suspense>
+				</ErrorProvider>
 			</div>
-			{newsItem && (
-				<NewsModal
-					news={newsItem}
-					onClose={() => {
-						setSelectedNews('');
-					}}
-				/>
-			)}
 		</>
 	);
 };
