6d5ba3bb0087464800a953927c76fffaddb8c2d0
jjipper <91707346+jjipper@users.noreply.github.com.>
2026-03-10 01:31:55 +0900

    refactor(issue): ErrorProvider, Suspense 적용을 위해 IssueDetailPage 내 상세 정보 영역 IssueDetailWrapper로 분리

--- src/features/Issues/index.ts
+++ src/features/Issues/index.ts
@@ -3,4 +3,5 @@ export { useIssueDetailQuery } from './hooks/useIssueDetailQuery';
 export { useIssuesQuery } from './hooks/useIssuesQuery';
 export { IssueCard } from './ui/IssueCard/IssueCard';
 export { IssueDetailCard } from './ui/IssueDetailCard/IssueDetailCard';
+export { IssueDetailWrapper } from './ui/IssueDetailWrapper/IssueDetailWrapper';
 export { IssueList } from './ui/IssueList/IssueList';
---
+++ src/features/Issues/ui/IssueDetailWrapper/IssueDetailWrapper.tsx
@@ -0,0 +1,17 @@
+import { useIssueDetailQuery } from 'features/Issues/hooks/useIssueDetailQuery';
+import { useParams } from 'react-router-dom';
+
+import { IssueDetailCard } from '../IssueDetailCard/IssueDetailCard';
+
+export const IssueDetailWrapper = () => {
+	const { id } = useParams();
+	const { data: issue } = useIssueDetailQuery(id ?? '');
+
+	if (!id) return <div>No Issue</div>;
+
+	return (
+		<ul className="flex flex-col gap-4">
+			<IssueDetailCard {...issue} />
+		</ul>
+	);
+};
