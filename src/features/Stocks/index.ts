dfd4b337dbed91552dbaad4f133e9ed0b7e18b79
jjipper <91707346+jjipper@users.noreply.github.com.>
2026-03-12 00:00:04 +0900

    refactor(stock): StockNewsListWrapper 파일 추가

--- src/features/Stocks/index.ts
+++ src/features/Stocks/index.ts
@@ -4,3 +4,4 @@ export { useStocksQuery } from './hooks/useStocksQuery';
 export { StockCard } from './ui/StockCard/StockCard';
 export { StockDetailCard } from './ui/StockDetailCard/StockDetailCard';
 export { StockList } from './ui/StockList/StockList';
+export { StockNewsListWrapper } from './ui/StockNewsListWrapper/StockNewsListWrapper';
---
+++ src/features/Stocks/ui/StockNewsListWrapper/StockNewsListWrapper.tsx
@@ -0,0 +1,52 @@
+import { SectionHeader, TwoColumnGrid } from 'features/layout';
+import {
+	NewsCard,
+	NewsModal,
+	useNewsDetailQuery,
+	useNewsQuery,
+} from 'features/News';
+import { useStocksDetailQuery } from 'features/Stocks';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+export const StockNewsListWrapper = () => {
+	const { ticker } = useParams();
+	const [selectedNews, setSelectedNews] = useState('');
+	const { data: stock } = useStocksDetailQuery(ticker ?? '');
+	const { data: newsList } = useNewsQuery(stock.newsList);
+	const { data: newsItem } = useNewsDetailQuery(selectedNews);
+
+	return (
+		<>
+			<div className="mt-6 flex flex-col gap-3">
+				<SectionHeader title="최신 뉴스" updateTime="2시간 전" />
+				{newsList ? (
+					<TwoColumnGrid>
+						{newsList.map((news) => {
+							return (
+								<NewsCard
+									key={news.id}
+									imageUrl={news.imageUrl}
+									title={news.title}
+									source={news.source}
+									publishedAt={news.publishedAt}
+									onClick={() => setSelectedNews(news.id)}
+								/>
+							);
+						})}
+					</TwoColumnGrid>
+				) : (
+					<p>'No relevant news available.'</p>
+				)}
+			</div>
+			{newsItem && (
+				<NewsModal
+					news={newsItem}
+					onClose={() => {
+						setSelectedNews('');
+					}}
+				/>
+			)}
+		</>
+	);
+};
