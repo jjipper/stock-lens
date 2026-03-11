d4e4424ea099b68a899037051b697555db5e7f03
jjipper <91707346+jjipper@users.noreply.github.com.>
2026-03-11 23:59:02 +0900

    refactor(news): NewsListWrapper 파일 NewsType에 따라서 분리

--- src/features/News/NewsListWrapper/NewsListWrapper.tsx
+++ src/features/News/NewsListWrapper/NewsListWrapper.tsx
@@ -1,50 +1,15 @@
-import { useIssueDetailQuery } from 'features/Issues';
-import { SectionHeader, TwoColumnGrid } from 'features/layout';
-import {
-	NewsCard,
-	NewsModal,
-	useNewsDetailQuery,
-	useNewsQuery,
-} from 'features/News';
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { IssueNewsListWrapper } from 'features/Issues';
+import { StockNewsListWrapper } from 'features/Stocks';
 
-export const NewsListWrapper = () => {
-	const { id } = useParams();
-	const [selectedNews, setSelectedNews] = useState('');
-	const { data: issue } = useIssueDetailQuery(id ?? '');
-	const { data: newsList } = useNewsQuery(issue.newsList);
-	const { data: newsItem } = useNewsDetailQuery(selectedNews);
-
-	if (!id) return <div>No Issue</div>;
-
+export const NewsListWrapper = ({
+	NewsType,
+}: {
+	NewsType: 'issue' | 'stock';
+}) => {
 	return (
 		<>
-			<div className="mt-6 flex flex-col gap-3">
-				<SectionHeader title="최신 뉴스" updateTime="2시간 전" />
-				<TwoColumnGrid>
-					{newsList.map((news) => {
-						return (
-							<NewsCard
-								key={news.id}
-								imageUrl={news.imageUrl}
-								title={news.title}
-								source={news.source}
-								publishedAt={news.publishedAt}
-								onClick={() => setSelectedNews(news.id)}
-							/>
-						);
-					})}
-				</TwoColumnGrid>
-			</div>
-			{newsItem && (
-				<NewsModal
-					news={newsItem}
-					onClose={() => {
-						setSelectedNews('');
-					}}
-				/>
-			)}
+			{NewsType == 'issue' ? <IssueNewsListWrapper /> : ''}
+			{NewsType == 'stock' ? <StockNewsListWrapper /> : ''}
 		</>
 	);
 };
