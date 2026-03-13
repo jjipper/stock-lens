e97f535f7fee37431a5fc01f2183d332a03165f2
jjipper <91707346+jjipper@users.noreply.github.com.>
2026-03-14 00:25:51 +0900

    fix(portal): 뒷배경 스크롤 막는 기능 임시 제거

--- src/features/shared/ui/Portal/Portal.tsx
+++ src/features/shared/ui/Portal/Portal.tsx
@@ -1,14 +1,14 @@
-import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 export const Portal = ({ children }: { children: React.ReactNode }) => {
-	useEffect(() => {
-		const originalOverflow = document.body.style.overflow;
-		document.body.style.overflow = 'hidden';
-		return () => {
-			document.body.style.overflow = originalOverflow;
-		};
-	}, []);
+	// TODO: 뉴스 모달에서만 스크롤 막히도록 작업 필요
+	// useEffect(() => {
+	// 	const originalOverflow = document.body.style.overflow;
+	// 	document.body.style.overflow = 'hidden';
+	// 	return () => {
+	// 		document.body.style.overflow = originalOverflow;
+	// 	};
+	// }, []);
 
 	return createPortal(
 		children,
