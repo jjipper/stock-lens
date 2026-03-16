8afc11d176a988b207bcb2371305071f7bcf30be
jjipper <91707346+jjipper@users.noreply.github.com.>
2026-03-17 00:37:39 +0900

    chore(issues): useIssuesQuery 제네릭 추론하도록 수정

--- src/features/Issues/hooks/useIssuesQuery.ts
+++ src/features/Issues/hooks/useIssuesQuery.ts
@@ -1,11 +1,10 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
-import type { IssueCardProps } from 'features/types/types';
 
 import { getIssues } from '../api/issuesAPI';
 import { ISSUE_QUERY_KEYS } from './queryKeys';
 
 export const useIssuesQuery = (limit?: number) => {
-	return useSuspenseQuery<IssueCardProps[]>({
+	return useSuspenseQuery({
 		queryKey: ISSUE_QUERY_KEYS.list(limit),
 		queryFn: () => getIssues(limit),
 	});
