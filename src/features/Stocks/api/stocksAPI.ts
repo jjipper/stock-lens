c3ac6a80d8fd5ac88cd0820ebd74c33156833b82
jjipper <91707346+jjipper@users.noreply.github.com.>
2026-03-17 00:37:16 +0900

    chore(stocks): useStocksQuery API에 제네릭 지정

--- src/features/Stocks/api/stocksAPI.ts
+++ src/features/Stocks/api/stocksAPI.ts
@@ -1,15 +1,16 @@
 import axios from 'axios';
+import type { StockCardProps } from 'features/types/types';
 
 export const stocksAPI = axios.create({
 	baseURL: '/stocks',
 });
 
 export const getStocks = async () => {
-	const res = await stocksAPI.get('/');
+	const res = await stocksAPI.get<StockCardProps[]>('/');
 	return res.data;
 };
 
 export const getStockDetail = async (ticker: string) => {
-	const res = await stocksAPI.get(`/${ticker}`);
+	const res = await stocksAPI.get<StockCardProps>(`/${ticker}`);
 	return res.data;
 };
--- src/features/Stocks/hooks/useStocksQuery.ts
+++ src/features/Stocks/hooks/useStocksQuery.ts
@@ -1,11 +1,10 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
-import type { StockCardProps } from 'features/types/types';
 
 import { getStocks } from '../api/stocksAPI';
 import { STOCK_QUERY_KEYS } from './queryKeys';
 
 export const useStocksQuery = (select?) => {
-	return useSuspenseQuery<StockCardProps[]>({
+	return useSuspenseQuery({
 		queryKey: STOCK_QUERY_KEYS.list,
 		queryFn: getStocks,
 		select,
