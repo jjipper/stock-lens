216557f907eed36344f587dc0746bac64af0ffb6
jjipper <91707346+jjipper@users.noreply.github.com.>
2026-03-17 00:33:47 +0900

    feat(issues): getIssuesInfinity API 추가

--- src/features/Issues/api/issuesAPI.ts
+++ src/features/Issues/api/issuesAPI.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import type { InfinityProps, IssueInfinityProps } from 'features/types/types';
 
 export const issuesAPI = axios.create({
 	baseURL: '/issues',
@@ -7,6 +8,21 @@ export const issuesAPI = axios.create({
 export const getIssues = async (limit?: number) => {
 	const res = await issuesAPI.get('/', {
 		params: { _limit: limit },
+	});
+	return res.data;
+};
+
+export const getIssuesInfinity = async ({
+	pageParam,
+}: {
+	pageParam: InfinityProps;
+}) => {
+	const { size, page } = pageParam;
+	const res = await issuesAPI.get<IssueInfinityProps>('/', {
+		params: {
+			_limit: size,
+			_page: page,
+		},
 	});
 	return res.data;
 };
