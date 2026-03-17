15e0126991d1034aa6fe9d9c90edc9bcbab9eb6b
jjipper <91707346+jjipper@users.noreply.github.com.>
2026-03-18 00:12:28 +0900

    feat(news): 뉴스 getNewsInfinityByIds API 추가

--- src/features/News/api/newsAPI.ts
+++ src/features/News/api/newsAPI.ts
@@ -1,4 +1,8 @@
 import axios from 'axios';
+import type {
+	NewsInfinityProps,
+	NewsListInfinityProps,
+} from 'features/types/types';
 
 export const newsAPI = axios.create({
 	baseURL: '/news',
@@ -7,6 +11,17 @@ export const newsAPI = axios.create({
 export const getNewsByIds = async (newsList: string[]) => {
 	const res = await newsAPI.get(
 		`?ids=${encodeURIComponent(newsList.join(','))}`,
+	);
+	return res.data;
+};
+
+export const getNewsInfinityByIds = async ({
+	newsList,
+	pageParam,
+}: NewsInfinityProps) => {
+	const { size, page } = pageParam;
+	const res = await newsAPI.get<NewsListInfinityProps>(
+		`?ids=${encodeURIComponent(newsList.join(','))}&_limit=${size}&_page=${page}`,
 	);
 	return res.data;
 };
