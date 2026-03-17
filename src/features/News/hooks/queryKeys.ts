7e5be99ed5415a2048033dd00a0d33c1c943d72c
jjipper <91707346+jjipper@users.noreply.github.com.>
2026-03-18 00:14:06 +0900

    feat(news): newsListInfinity 쿼리키 추가

--- src/features/News/hooks/queryKeys.ts
+++ src/features/News/hooks/queryKeys.ts
@@ -1,4 +1,5 @@
 export const NEWS_QUERY_KEYS = {
 	newsList: (newsList: string[]) => ['news', newsList] as const,
+	newsListInfinity: ['news', 'infinity'] as const,
 	newsItem: (id: string) => ['news', id] as const,
 };
