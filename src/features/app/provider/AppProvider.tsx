b3ea5039214f2bd454cc7031e5b530f1a41f1ad9
jjipper <91707346+jjipper@users.noreply.github.com.>
2026-03-20 03:21:22 +0900

    feat(toast): AppProvider에 ToastProvider 추가

--- src/features/app/provider/AppProvider.tsx
+++ src/features/app/provider/AppProvider.tsx
@@ -1,14 +1,20 @@
-import { Loading } from 'features/shared';
+import { Loading, Toast } from 'features/shared';
 import { type ReactNode, Suspense } from 'react';
 
 import { ReactQueryProvider } from '../provider/ReactQueryProvider';
 import { ErrorProvider } from './ErrorProvider';
+import { ToastProvider } from './ToastProvider';
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
 	return (
 		<ReactQueryProvider>
 			<ErrorProvider>
-				<Suspense fallback={<Loading />}>{children}</Suspense>
+				<Suspense fallback={<Loading />}>
+					<ToastProvider>
+						<Toast />
+						{children}
+					</ToastProvider>
+				</Suspense>
 			</ErrorProvider>
 		</ReactQueryProvider>
 	);
