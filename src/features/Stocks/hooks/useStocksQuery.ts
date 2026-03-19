929b83b70d2733f0048af20a5fe1b661dd67f398
jjipper <91707346+jjipper@users.noreply.github.com.>
2026-03-20 01:54:07 +0900

    chore(stocks): useStocksQuery 타입 정의 추가

--- src/features/Stocks/hooks/useStocksQuery.ts
+++ src/features/Stocks/hooks/useStocksQuery.ts
@@ -1,9 +1,12 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
+import type { StockCardProps } from 'features/types/types';
 
 import { getStocks } from '../api/stocksAPI';
 import { STOCK_QUERY_KEYS } from './queryKeys';
 
-export const useStocksQuery = (select?) => {
+export const useStocksQuery = <TSelected = StockCardProps[]>(
+	select?: (data: StockCardProps[]) => TSelected,
+) => {
 	return useSuspenseQuery({
 		queryKey: STOCK_QUERY_KEYS.list,
 		queryFn: getStocks,
