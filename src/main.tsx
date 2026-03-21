c41f111fa40f30358fee0d69953af2b7cfbb3b2e
1ilsang <1ilsang.dev@gmail.com>
2026-03-21 14:29:39 +0900

    feat: Build 배포 준비

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 async function enableMocking() {
-	if (!import.meta.env.DEV) {
-		return;
-	}
+	// NOTE: 서버가 없으므로 프로덕션 환경에서 워커 실행
+	// if (!import.meta.env.DEV) {
+	// 	return;
+	// }
 
 	const { worker } = await import('./mocks/browser');
 	await worker.start();
